537e200a7b5872144d73c156260121844e2dd93f
Viwe <4114637@myuwc.ac.za>
2021-04-21 14:13:42 +0200

    Heat_Map_Excecise

---
+++ Heatmap_Excesize_Viwe.R
@@ -0,0 +1,37 @@
+#Author: Viwe_Mabongo
+#Date: 21 April 2021
+#Heatmap_Excercise
+
+
+
+
+library(tidyverse)
+library(corrplot)
+library(ggpubr)
+library(reshape)
+library(dplyr)
+
+
+
+#load data
+
+ecklonia <- read_csv("ecklonia.csv")
+
+#Selecting columns that must be included in the dataset.
+ecklonia_sub1 <- ecklonia %>% 
+  select(-species, -site, -ID)
+
+#Perfom Pearson test
+data <- round(cor(ecklonia_sub1), 2)
+
+#melting to organize the data
+Melted_data <- melt(data)
+
+#Create ggplot for Heatmap
+ggplot(data = Melted_data, aes(x = X1, y = X2, fill = value)) +
+  geom_tile() +
+  theme(axis.text = element_text(angle = 45))
+
+
+
+
