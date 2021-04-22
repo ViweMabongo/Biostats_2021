c1f88ea1ded5f085f0799e1d7362e9d33b5339e5
Viwe <4114637@myuwc.ac.za>
2021-04-22 12:51:59 +0200

    Quiz_2

---
+++ biostats day 2.R
@@ -0,0 +1,126 @@
+#Author: Viwe Mabongo
+#Date: 20 April 2021
+#Biostatiscs
+
+
+
+
+
+
+
+library(tidyverse)
+library(plotly)
+library(ggpubr)
+
+# Random normal data
+set.seed(666)
+r_dat <- data.frame(dat = c(rnorm(n = 1000, mean = 10, sd = 3),
+                            rnorm(n = 1000, mean = 8, sd = 2)),
+                    sample = c(rep("A", 1000), rep("B", 1000)))
+
+# Create histogram for r_dat.
+h <- ggplot(data = r_dat, aes(x = dat, fill = sample)) +
+  geom_histogram(position = "dodge", binwidth = 1, alpha = 0.8) +
+  geom_density(aes(y = 1*..count.., fill = sample), colour = NA, alpha = 0.4) +
+  labs(x = "value")
+h
+
+shapiro.test(r_dat$dat) #Checking normality, is it normally distributed.
+
+# To make the data to be in a tidy distributed way
+r_dat %>% 
+  group_by(sample) %>% 
+  summarise(norm_dat = as.numeric(shapiro.test(dat)[2]))
+
+r_dat %>% # homoscedasticity
+  group_by(sample) %>% 
+  summarise(sample_var = var(dat))# variance of the samples we are comparing should not be more than two to four times greater than one another
+
+# To check two assumptions of normality and homoscedasticty simultaneously.
+two_assum <- function(x) {
+  x_var <- var(x)
+  x_norm <- as.numeric(shapiro.test(x)[2])
+  result <- c(x_var, x_norm)
+  return(result)
+}
+
+
+r_dat %>% 
+  group_by(sample) %>% 
+  summarise(sample_var = two_assum(dat)[1],
+            sample_norm = two_assum(dat)[2])
+# create a single sample of random normal data, comparing one sample data to a known population mean.
+r_one <- data.frame(dat = rnorm(n = 20, mean = 20, sd = 5),
+                    sample = "A")
+
+shapiro.test(r_one$dat)
+
+# compare random data against a population mean of 20
+t.test(r_one$dat, mu = 20)
+
+r_two <- data.frame(dat = c(rnorm(n = 20, mean = 4, sd = 1),
+                            rnorm(n = 20, mean = 5, sd = 1)),
+                    sample = c(rep("A", 20), rep("B", 20)))
+# compare random data against a population mean of 30
+t.test(r_one$dat, mu = 30)
+
+#Create a plot for better data visualization
+ggplot(data = r_one, aes(y = dat, x = sample)) +
+  geom_boxplot(fill = "lightsalmon") +
+  # population  mean (mu) = 20
+  geom_hline(yintercept = 20, colour = "blue", 
+             size = 1, linetype = "dashed") +
+  # population  mean (mu) = 30
+  geom_hline(yintercept = 30, colour = "red", 
+             size = 1, linetype = "dashed") +
+  labs(y = "Value", x = NULL) +
+  coord_flip()
+
+# perform t-test
+# note how we set the `var.equal` argument to TRUE because we know 
+# our data has the same SD (they are simulated as such!)
+t.test(dat ~ sample, data = r_two, var.equal = TRUE)
+
+
+ecklonia <- read_csv("data/ecklonia.csv") %>% 
+  gather(key = "variable", value = "value", -species, -site, -ID)
+
+
+ggplot(data = ecklonia, aes(x = variable, y = value, fill = site)) +
+  geom_boxplot() +
+  coord_flip()
+
+# filter the data
+ecklonia_sub <- ecklonia %>% 
+  filter(variable == "stipe_mass")
+
+# then create a new figure
+ggplot(data = ecklonia_sub, aes(x = variable, y = value, fill = site)) +
+  geom_boxplot() +
+  coord_flip() +
+  labs(y = "stipe mass (kg)", x = "") +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
+
+# First grab the data
+chicks <- as_tibble(ChickWeight)
+
+# Then subset out only the sample sets to be compared
+chicks_sub <- chicks %>% 
+  filter(Diet %in% c(1, 2), Time == 21)
+
+compare_means(weight ~ Diet, data = chicks_sub, method = "t.test")
+
+
+chicks.aov1 <- aov(weight ~ Diet, data = filter(chicks, Time == 21))
+summary(chicks.aov1)
+
+# Then load data
+chicks <- as_tibble(ChickWeight)
+chicks %>% 
+  filter(Time == 0) %>% 
+  group_by(Diet) %>% 
+  summarise(norm_wt = as.numeric(shapiro.test(weight)[2]),
+            var_wt = var(weight))
+
+compare_means(weight ~ Diet, data = filter(chicks, Time == 0, Diet %in% c(1, 2)), method = "wilcox.test")
